3856d6eeac7faf0f2c1ed6e5e3a252c71f0f657f
Yan, Qun <yq1119apzz@gmail.com>
2017-07-13 18:31:50 -0400

    add initial component for footer header and home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PictureComponent } from './home/picture/picture.component';
+import { DescriptionListComponent } from './home/description-list/description-list.component';
+import { DescriptionComponent } from './home/description-list/description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PictureComponent,
+    DescriptionListComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule
