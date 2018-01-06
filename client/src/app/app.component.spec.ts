3d341ff35528bd6c1a75b94746d4288951a343e9
Jackson7683 <jie.chen1116@gmail.com>
2018-01-06 16:46:43 -0500

    add initial unit tests for all components.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
       ],
     }).compileComponents();
   }));
@@ -17,16 +24,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+  it('should create the header component', async(() => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    const header = fixture.debugElement.componentInstance;
+    expect(header).toBeTruthy();
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+  it('should create the footer component', async(() => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    const footer = fixture.debugElement.componentInstance;
+    expect(footer).toBeTruthy();
   }));
 });
--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -1,25 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { PictureComponent } from './picture/picture.component';
+import { DescriptionListComponent } from './description-list/description-list.component';
+import { DescriptionComponent } from './description-list/description/description.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent, PictureComponent, DescriptionListComponent, DescriptionComponent ]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should create the home component', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const home = fixture.debugElement.componentInstance;
+    expect(home).toBeTruthy();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('should create the picture component', () => {
+    const fixture = TestBed.createComponent(PictureComponent);
+    const picture = fixture.debugElement.componentInstance;
+    expect(picture).toBeTruthy();
   });
+
+  it('should create the description-list component', async(() => {
+    const fixture = TestBed.createComponent(DescriptionListComponent);
+    const description_list = fixture.debugElement.componentInstance;
+    expect(description_list).toBeTruthy();
+  }));
 });
